dac99117970a7ec2783fef84015d3f2b861de475
kleberchiliquinga <kachiliquingal@uce.edu.ec>
2025-11-06 20:56:45 -0500

    style(codebase): 🧹 remove all code comments for cleaner structure

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,11 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-// Endpoint que obtiene los personajes desde la API pública de Rick and Morty
 app.get('/api/personajes', async (req, res) => {
     try {
         const response = await fetch('https://rickandmortyapi.com/api/character');
         const data = await response.json();
-        res.json(data.results); // puedes enviar solo los resultados
+        res.json(data.results);
     } catch (error) {
         console.error('Error al obtener personajes:', error);
         res.status(500).json({ error: 'Error al obtener los datos' });
